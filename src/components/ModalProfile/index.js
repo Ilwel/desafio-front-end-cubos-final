97bf3c3719709afb9c604e7131b91b5999a22d04
ilwel <ilwelisaac@hotmail.com>
2021-09-30 15:44:24 -0300

    logout implementation

--- src/components/ModalProfile/index.js
+++ src/components/ModalProfile/index.js
@@ -2,10 +2,12 @@ import './styles.css'
 import editIcon from '../../assets/edit_icon.svg'
 import logoutIcon from '../../assets/logout_icon.svg'
 import ModalEditProfile from '../ModalEditProfile'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import AuthContext from '../../contexts/AuthContext'
 
 export default function ModalProfile(props) {
   const [open, setOpen] = useState(false);
+  const { setToken } = useContext(AuthContext);
 
   function handleClickProfile() {
     setOpen(!open);
@@ -20,7 +22,7 @@ export default function ModalProfile(props) {
               <img src={editIcon} alt="" />
               Editar
             </div>
-            <div className="c-modal-profile__item last">
+            <div onClick={() => setToken('')} className="c-modal-profile__item last">
               <img src={logoutIcon} alt="" />
               Sair
             </div>
