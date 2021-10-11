474fec585e823ac9275609923b46b81d109f56e5
ilwel <ilwelisaac@hotmail.com>
2021-10-11 02:54:43 -0300

    format functions

---
+++ src/utils/useCurrencyFormat.js
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react'
+import Inputmask from "inputmask";
+
+const useCurrencyMask = (register, options) => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if (!ref.current && !register) return
+    else {
+
+      Inputmask({
+        ...options,
+        alias: 'currency',
+        numericInput: true,
+        rightAlign: false,
+        positionCaretOnClick: "none"
+
+      }).mask(ref.current)
+
+      register('value', ref.current);
+
+    }
+
+
+  }, [register, options])
+
+  return ref
+}
+
+export default useCurrencyMask
